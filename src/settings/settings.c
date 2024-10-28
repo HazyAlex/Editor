8993a2bc06999495b227d50b65169f6d5ec0867c
HazyAlex <HazyAlex@protonmail.com>
2024-10-28 21:07:14 +0000

    feat: add window/font settings

--- src/settings/settings.c
+++ src/settings/settings.c
@@ -1,24 +1,30 @@
 #include "settings/settings.h"
+#include <string.h>
 
 
-Settings initialize_settings()
+Settings initialize_settings(void)
 {
     Settings settings;
 
-    settings.starting_window_width = 800;
-    settings.starting_window_height = 600;
+    settings.starting_window_width = 2560;
+    settings.starting_window_height = 1440;
     settings.window_swap_interval_active = 0;
     settings.window_swap_interval_inactive = 60;
+
     settings.window_name = string_from_c("Editor");
+    settings.window_fullscreen = TRUE;
+    settings.window_monitor_index = 0;
+
+    settings.font = string_from_c("assets/fonts/clacon2.ttf");
+    settings.font_size = 32;
 
     return settings;
 }
 
 void free_settings(Settings *settings)
 {
+    string_dealloc(&settings->font);
     string_dealloc(&settings->window_name);
 
     memset(settings, 0, sizeof(Settings));
 }
-
-
--- src/settings/settings.h
+++ src/settings/settings.h
@@ -1,20 +1,25 @@
 #ifndef SETTINGS_INCLUDE_GUARD
 #define SETTINGS_INCLUDE_GUARD
 
-#include "string/string.h"
 #include "structures/types.h"
+#include "string/string.h"
 
 
 typedef struct {
     i32 starting_window_width;
     i32 starting_window_height;
 
+    String window_name;
+    bool window_fullscreen; // 0 - windowed, 1 - fullscreen
+    i8 window_monitor_index; // monitor index (fullscreen only)
     i32 window_swap_interval_active;
     i32 window_swap_interval_inactive;
-    String window_name;
+
+    String font;
+    u32 font_size;
 } Settings;
 
-Settings initialize_settings();
+Settings initialize_settings(void);
 void free_settings(Settings *settings);
 
 
