ae796b6e22c298718417472ac5db71efb6dbce24
HazyAlex <HazyAlex@protonmail.com>
2024-10-28 21:00:01 +0000

    feat(types): add color type and more vector types

--- src/structures/types.h
+++ src/structures/types.h
@@ -4,6 +4,10 @@
 #include <stdint.h>
 
 
+typedef _Bool bool;
+#define TRUE  1
+#define FALSE 0
+
 typedef uint64_t u64;
 typedef uint32_t u32;
 typedef uint16_t u16;
@@ -13,6 +17,56 @@ typedef int64_t i64;
 typedef int32_t i32;
 typedef int16_t i16;
 typedef int8_t  i8;
+
+
+typedef union {
+    struct {
+        i32 x;
+        i32 y;
+    };
+    i32 data[2];
+} ivec2;
+
+typedef union {
+    struct {
+        i32 x;
+        i32 y;
+        i32 z;
+    };
+    i32 data[3];
+} ivec3;
+
+typedef union {
+    struct {
+        u32 x;
+        u32 y;
+    };
+    u32 data[2];
+} uvec2;
+
+typedef union {
+    struct {
+        u32 x;
+        u32 y;
+        u32 z;
+    };
+    u32 data[3];
+} uvec3;
+
+typedef union {
+    struct {
+        u8 r;
+        u8 g;
+        u8 b;
+    };
+    u8 data[3];
+} color;
+
+ivec2 vec2_from_i32(i32 x, i32 y);
+ivec3 vec3_from_i32(i32 x, i32 y, i32 z);
+uvec2 vec2_from_u32(u32 x, u32 y);
+uvec3 vec3_from_u32(u32 x, u32 y, u32 z);
+color color_from(u8 r, u8 g, u8 b);
 
 
 #endif
